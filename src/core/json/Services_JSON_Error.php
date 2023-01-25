95b4873e8b8d83525f9636a4ee69555fbdd75989
ankio <admin@ankio.net>
2023-01-25 23:24:39 +0800

    feat: 移除不必要的lang()

--- src/core/json/Services_JSON_Error.php
+++ src/core/json/Services_JSON_Error.php
@@ -9,7 +9,7 @@ class Services_JSON_Error extends Exception
 {
     function __construct($message = 'unknown error')
     {
-        Log::record("JSON", lang("JSON数据解析错误：%s", $message));
+        Log::record("JSON", "JSON数据解析错误：%s", $message);
         parent::__construct($message, 0, null);
     }
 }
